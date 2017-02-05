8d9d9ec1c44cbe2e21506083b7aa806201842d3d
Matyas Tomolya <tomolya.matyas@gmail.com>
2017-02-05 13:54:30 +0100

    View 2 missing functionality added

--- js/view/secondView.js
+++ js/view/secondView.js
@@ -5,6 +5,19 @@ var SecondView = function (container, model) {
   	// and/or ones that responed to interaction)
 	this.numberOfGuests = container.find("#numberOfGuests");
 	this.numberOfGuests.val(model.getNumberOfGuests());
+
+	var selectedDish = model.getSelectedDish("starter");
+	if (selectedDish != undefined) {
+		var price = 0;
+		for (var i = 0; i < selectedDish.ingredients.length; i++) {
+			price = selectedDish.ingredients[i].price + price;
+		}
+		container.find("#pendingCost").html(price);
+	}
+	else {
+		container.find("#pendingCost").html("0.00");
+	}
+	container.find("#totalCost").html(model.getTotalMenuPrice());
 	
 }
  
